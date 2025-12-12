173f24da312d43aad7299ee54c44847819c45131
edwin <musyokaedwin01@gmail.com>
2025-12-12 22:50:52 +0300

    Analyzing Data with Modern Syntax part 1

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
+require('datejs')
 
+function combineUsers(...args) {
+  const combineObjects ={users:[]}
 
+  for(const arr of args) {
+    combineObjects.users.push(...arr)
+  }
+
+  combineObjects.merge_date = new Date().toString("M/d/yyyy")
+
+  return combineObjects;
+
+}
+console.log(combineUsers)
 
 module.exports = {
   ...(typeof combineUsers !== 'undefined' && { combineUsers })
